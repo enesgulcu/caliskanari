b0ce309becf98f6e6b8fb92bb7ee119d4406676e
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-02-15 12:09:14 +0300

    Tailwind, Klasörler Oluşturuldu. Temizleme Yapıldı

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,13 @@
-import './globals.css'
+import '@/styles/global.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="tr">
+
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
