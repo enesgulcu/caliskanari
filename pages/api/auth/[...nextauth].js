427cccd8ab5f68f4106ddbef2a4e98b09952f35b
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-04-15 10:06:08 +0300

    test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,36 +1,28 @@
 import NextAuth from "next-auth"
-import CredentialsProvider, { CredentialsConfig } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import {postAPI} from "@/services/fetchAPI";
 
-
-
-
-console.log("[...nextauth] ########################## 1")
 let loginPageRoute = "student";
 
-const authOptions:any = {
+export const authOptions = {
+
   providers: [
-    // CredentialsProvider ile email ve şifreyi kullanıcıdan alarak normal giriş yapmasını sağlarız.
+    // CredentialsProvider ile email veşifreyi kullanıcıdan alarak normal giriş yapmasını sağlarız.
     // farklı giriş yöntemleri ile (google - github - facebook) giriş için hazır "provider" ları kullanabiliriz.
     CredentialsProvider({
       name: "Credentials",
+
       credentials: {
         email: { label: "email", type: "text"},
         password: { label: "Password", type: "password" },
         role: { label: "role", type: "text" },
       },
-
-      async authorize(credentials): Promise<any> {
-        console.log("[...nextauth] ########################## 2")
+      async authorize(credentials, req) {
         // kontrol edilecek (email ve password) bilgilerini credentials değişkeninden alıyoruz.
-        const { email, password, role}:any = credentials;
+        const { email, password, role} = credentials;
         // giriş yapılacak sayfayı role değişkeninden alıyoruz.
         loginPageRoute = role;
-        console.log("[...nextauth] ########################## 3")
-        console.log(email)
-        console.log(password)
-        console.log(role)
-        console.log("[...nextauth] ########################## 4")
+
         if(email){
           // yukarıda aldığımız giriş bilgilerini => [email eşleşmesi, password doğrulaması] için fonksiyonumuza gönderiyoruz.
           const data = await postAPI(`/auth/login`, {role, email, password});
@@ -43,11 +35,9 @@ const authOptions:any = {
             }
           }
 
-          
-
           const {userFromDB, success, error, status, verifyEmail} = data;
             if(userFromDB === null || !success || userFromDB === undefined || error || !userFromDB){
-              let error2:any = new  Error();
+              let error2 = new Error();
                   error2.message = error;
                   error2.status = status;
                   error2.verifyEmail = verifyEmail;
@@ -65,47 +55,38 @@ const authOptions:any = {
                 return user;
             }
         }
-
         else{
           throw new Error("Giriş işleminde bir hata oluştu.");
         }        
       }
     }),
   ],
-
   secret: process.env.NEXTAUTH_SECRET,
-
   jwt: {
     secret: process.env.NEXTAUTH_SECRET,
     encryption: true, 
-
   },
-
   // kullanıcı giriş yaptıktan sonra giriş yapan kullanıcının bilgilerini token değişkenine atıyoruz.
   session: {
     strategy: 'jwt',
     maxAge: 1 * 24 * 60 * 60 // 1 days * 24 hours * 60 minutes * 60 seconds
   },
-
   callbacks: {
     // jwt fonksiyonu ile kullanıcı giriş yaptıktan sonra giriş yapan kullanıcının bilgilerini token değişkenine atıyoruz.
     // bu bilgileri session fonksiyonunda kullanacağız.
-    async jwt({ token, user }:{token:any, user:any}) {
+    async jwt({ token, user }) {
       return { ...token, ...user };
     },
     // session fonksiyonu ile kullanıcı giriş yaptıktan sonra giriş yapan kullanıcının bilgilerini session değişkenine atıyoruz.
-    async session({ session, token }:any) {
+    async session({ session, token }) {
       session.user = token;
       return session;
     },
   },
-
   pages:{
     // signIn fonksiyonu çalıştığında kulanıcıyı yönlendireceğimiz sayfayı belirtiyoruz.
     signIn: `/auth/login/${loginPageRoute}`,
     encryption: true,
   },
 }
-
 export default NextAuth(authOptions)
-
