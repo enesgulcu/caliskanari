6f0901b98f19dac092af5a938eda20834160a11b
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-04-17 18:15:44 +0300

    any düzeltmeleri devam ediyor

--- pages/api/test/index.tsx
+++ pages/api/test/index.tsx
@@ -3,11 +3,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const handler = async (req:NextApiRequest, res:NextApiResponse): Promise<void> => {
 
     if(req.method === "POST") {
-        console.log("Method POST")
         return res.status(200).json({message: "Method POST"})
     }
     if(req.method === "GET") {
-        console.log("Method GET")
+
         return res.status(200).json({message: "Method GET"})
     }
 
