804d8975a997b663ef5b7006c9eada392042b3ef
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-04-20 11:49:21 +0300

    test

--- app/auth/verifyEmail/page.tsx
+++ app/auth/verifyEmail/page.tsx
@@ -1,15 +1,8 @@
 import VerifyEmailContainer from '@/containers/auth/verifyEmail';
 
-interface Props {
-  searchParams:{
-    key: string;
-    email: string;
-    role: string;
-    time:number
-  }
-}
 
-const VerifyEmailPage = async ({searchParams}: Props) : Promise<JSX.Element> => {
+
+const VerifyEmailPage = async ({searchParams}: any) : Promise<JSX.Element> => {
 
   const verifyEmailContainer = await VerifyEmailContainer({ searchParams });
     
