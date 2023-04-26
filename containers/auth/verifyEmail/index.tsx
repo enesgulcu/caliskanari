088e7e710c36120125a54972ed7ae522d267b443
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-04-26 12:49:26 +0300

    tüm işlemler build aşamasında sorunsuz çalışıyor.

--- containers/auth/verifyEmail/index.tsx
+++ containers/auth/verifyEmail/index.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic'
-
 import React from 'react'
 import VerifyEmailComponents from '@/components/auth/verifyEmail';
 import { postAPI } from '@/services/fetchAPI'
--- functions/auth/verifyEmail/index.tsx
+++ functions/auth/verifyEmail/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 
-const VerifyEmail = async ({key, email, role}:Props): Promise<{status: string, message: string} | {error:any}> =>{
+const VerifyEmail = async ({key, email, role}:Props): Promise<{status?: string, message?: string, error?:any} | {error:any}> =>{
    try {
     const verifyEmailData = await getDataByUnique("VerifyEmail", {secretKey: key});
      if(!verifyEmailData || verifyEmailData.error  || verifyEmailData == null) {
