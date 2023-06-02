4f14e61b56515bf14b93f1db6712cb60b2b493cc
Enes Gülcü <enes.gulcu@hotmail.com>
2023-06-02 22:59:48 +0300

    code düzeltmesi yapıldı

--- components/dashboard/panels/admin/slider/index.tsx
+++ components/dashboard/panels/admin/slider/index.tsx
@@ -49,10 +49,6 @@ const Slider:React.FC = () => {
     const [buttonText, setButtonText] = useState<string>();
     const [buttonLink, setButtonLink] = useState<string>();
 
-    const [isActiveMainText, setIsActiveMainText] = useState<boolean>();
-    const [isActiveSubText, setIsActiveSubText] = useState<boolean>();
-    const [isActiveButtonText, setIsActiveButtonText] = useState<boolean>();
-
     const [bgImage, setBgImage] = useState<string>("");
     const [bgImageMd, setBgImageMd] = useState<string>("");
     const [bgImageLg, setBgImageLg] = useState<string>("");
@@ -198,20 +194,20 @@ const Slider:React.FC = () => {
                                     <div className='flex justify-center items-end content-center gap-4'>
                                         
                                         {/* SWITCH BUTTON */}
-                                        <div onClick={() => setIsActiveMainText(!isActiveMainText)} className={`relative hover:cursor-pointer p-2 border-gray-500   shadow rounded-xl pl-4 ${isActiveMainText ? "bg-blue-400" : "bg-red-400"}`}>
+                                        <div onClick={() => setMainTextOpen(!MainTextOpen)} className={`relative hover:cursor-pointer p-2 border-gray-500   shadow rounded-xl pl-4 ${MainTextOpen ? "bg-blue-400" : "bg-red-400"}`}>
                                             <input
                                                 className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-white before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-200 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
                                                 type="checkbox"
-                                                name="isActiveMainText"
-                                                checked={isActiveMainText ? isActiveMainText : false}
+                                                name="MainTextOpen"
+                                                checked={MainTextOpen ? MainTextOpen : false}
                                                 onChange={(e)=>{
                                                     props.handleChange({
                                                         target: {
-                                                        name: "isActiveMainText",
+                                                        name: "MainTextOpen",
                                                         value: e.target.checked,
                                                         },
                                                     });
-                                                    setIsActiveMainText(e.target.checked);
+                                                    setMainTextOpen(e.target.checked);
                                                     }                                                
                                                 }
                                             />
@@ -250,20 +246,20 @@ const Slider:React.FC = () => {
                                     <div className='flex justify-center items-end content-center gap-4'>
                                         
                                         {/* SWITCH BUTTON */}
-                                        <div  onClick={() => setIsActiveSubText(!isActiveSubText)} className={`relative hover:cursor-pointer p-2 border-gray-500   shadow rounded-xl pl-4 ${isActiveSubText ? "bg-blue-400" : "bg-red-400"}`}>
+                                        <div  onClick={() => setSubTextOpen(!subTextOpen)} className={`relative hover:cursor-pointer p-2 border-gray-500   shadow rounded-xl pl-4 ${subTextOpen ? "bg-blue-400" : "bg-red-400"}`}>
                                             <input
                                                 className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-white before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-200 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
                                                 type="checkbox"
-                                                name="isActiveSubText"
-                                                checked={isActiveSubText ? isActiveSubText : false}
+                                                name="subTextOpen"
+                                                checked={subTextOpen ? subTextOpen : false}
                                                 onChange={(e)=>{
                                                     props.handleChange({
                                                         target: {
-                                                        name: "isActiveSubText",
+                                                        name: "subTextOpen",
                                                         value: e.target.checked,
                                                         },
                                                     });
-                                                    setIsActiveSubText(e.target.checked);
+                                                    setSubTextOpen(e.target.checked);
                                                     }                                                
                                                 }
                                             />
@@ -300,20 +296,20 @@ const Slider:React.FC = () => {
                                     {/* BUTTON TEXT  START*/}
                                     <div className='flex justify-center items-end content-center gap-4'>
                                         {/* SWITCH BUTTON */}
-                                        <div onClick={() => setIsActiveButtonText(!isActiveButtonText)} className={`relative hover:cursor-pointer p-2 border-gray-500   shadow rounded-xl pl-4 ${isActiveButtonText ? "bg-blue-400" : "bg-red-400"}`}>
+                                        <div onClick={() => setButtonOpen(!buttonOpen)} className={`relative hover:cursor-pointer p-2 border-gray-500   shadow rounded-xl pl-4 ${buttonOpen ? "bg-blue-400" : "bg-red-400"}`}>
                                             <input
                                                 className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-white before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-200 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
                                                 type="checkbox"
-                                                name="isActiveButtonText"
-                                                checked={isActiveButtonText ? isActiveButtonText : false}
+                                                name="buttonOpen"
+                                                checked={buttonOpen ? buttonOpen : false}
                                                 onChange={(e)=>{
                                                     props.handleChange({
                                                         target: {
-                                                        name: "isActiveButtonText",
+                                                        name: "buttonOpen",
                                                         value: e.target.checked,
                                                         },
                                                     });
-                                                    setIsActiveButtonText(e.target.checked);
+                                                    setButtonOpen(e.target.checked);
                                                     }                                                
                                                 }
                                             />
