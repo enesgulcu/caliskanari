9ade382facec27af3b4bb5de1a492e3b7cde8bb7
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-11-07 15:27:48 +0300

    Typescript convert to JSX

--- app/dashboard/student/page.tsx
+++
@@ -1,7 +0,0 @@
-const Student:React.FC = () => {
-  return (
-    <div>Student Main Page</div>
-  )
-}
-
-export default Student
--- app/dashboard/teacher/page.tsx
+++
@@ -1,7 +0,0 @@
-const Teacher:React.FC = () => {
-  return (
-    <div>Teacher Main Page</div>
-  )
-}
-
-  export default Teacher
